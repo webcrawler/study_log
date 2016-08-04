9cdf3f3c42ea631f1139ec7091cbc2f5df4bb7f8
fjut <1274219974@qq.com>
2016-08-04 16:25:52 +0800

    unity3d study log

---
+++ unity3d_log.h
@@ -0,0 +1,6 @@
+// create by fjut 2016.08.04
+
+1. particle 及时播放，设置：Open the "Emission" settings in your particle system, set the "Rate" to 0, c
+lick the "+" at the right of "Burst" and set the burst time to "0.00".
+2. 粒子系统材质的texture POT textures，否则ios上有问题。
+3. 
