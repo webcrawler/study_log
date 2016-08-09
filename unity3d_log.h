c37dd63f74d557977ac83fbd4e894209dc2157be
fjut <1274219974@qq.com>
2016-08-09 16:07:21 +0800

    设置图片Format为Truecolor
    设置图片Format为Truecolor

--- unity3d_log.h
+++ unity3d_log.h
@@ -3,4 +3,4 @@
 1. particle 及时播放，设置：Open the "Emission" settings in your particle system, set the "Rate" to 0, c
 lick the "+" at the right of "Burst" and set the burst time to "0.00".
 2. 粒子系统材质的texture POT textures，否则ios上有问题。
-3. 
+3. 有些渐变颜色的图片在iOS上出现块状，设置图片Format为Truecolor
